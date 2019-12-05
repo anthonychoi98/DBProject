0122b035b1e0926c442414e723b45920b928c7f8
Anthony Choi <froshiga@gmail.com>
2019-12-04 21:26:28 -0800

    second commit

--- tennis.py
+++ tennis.py
@@ -15,21 +15,62 @@ def home():
 
 @app.route('/players', methods=['POST', 'GET'])
 def players():
-    fn = ''
-    ln = ''
-    data = ''
+    fn=''
+    ln=''
+    data=''
+    playerID=''
+    fn=''
+    ln=''
+    bd=''
+    country=''
+    mwon=''
+    gwon=''
 
     if request.method == 'POST':
         fn =  request.form.getlist('fn')
         ln =  request.form.getlist('ln')
-
+        #get player info
         query = "select * from players where p_firstName = '%s' AND p_lastName = '%s';"%(fn[0], ln[0])
         print(query)
         conn = create_connection()
-        data = execute_query(conn, query)
-        close_connection(conn)
 
-    return render_template("players.html", data=data)
+        crs = conn.cursor()
+        crs.execute(query)
+
+        data = [[str(item) for item in results] for results in crs.fetchall()]
+
+        #get player ID
+        query2 = "select MIN(p_playerID) from players where p_firstName = '%s' AND p_lastName = '%s';"%(fn[0], ln[0])
+        playerID = crs.execute(query).fetchone()[0]
+
+        print(playerID)
+
+        #get titles won
+        query2 = "select COUNT(*) from allTournaments where v_wID = %d AND v_round = 'F';"%(int(playerID))
+        query3 = "select COUNT(*) from allTournaments A, tournaments T where v_wID = %d AND v_round = 'F' AND t_ID = A.v_ID AND t_level = 'M';"%(int(playerID))
+        query4 = "select COUNT(*) from allTournaments A, tournaments T where v_wID = %d AND v_round = 'F' AND t_ID = A.v_ID AND t_level = 'G';"%(int(playerID))
+
+        twon = crs.execute(query2).fetchone()[0]
+        mwon = crs.execute(query3).fetchone()[0]
+        gwon = crs.execute(query4).fetchone()[0]
+        print("won: " + str(twon))
+        print("mwon: " + str(mwon))
+        print("gwon: " + str(gwon))
+
+        #get grand slams won
+        #get total titles
+        close_connection(conn)
+        s = data[0]
+        print(s)
+        li = str(s).strip(']').split(", ")
+        fn =li[1]
+        ln = li[2]
+        bd=li[3]
+        country=li[5]
+
+
+
+    return render_template("players.html",fn=fn, ln=ln,bd=bd,country=country, gwon=gwon, mwon=mwon)
 
 def create_connection():
     conn = None
@@ -44,8 +85,6 @@ def execute_query(conn, query):
     if query == '' : return
 
     #print (query)
-
-
 
     cursor = conn.cursor()
 
