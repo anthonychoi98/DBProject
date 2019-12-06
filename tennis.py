aaf9c3bbce072a1ee1af871fd2fbec4e8c202e6a
Anthony Choi <froshiga@gmail.com>
2019-12-05 23:31:06 -0800

    changed from post to get

--- tennis.py
+++ tennis.py
@@ -3,6 +3,7 @@ from flask import request, jsonify, url_for, render_template
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users/anthonychoi98/Documents/GitHub/TennisDB/Tennis/Tennis.db'
@@ -13,8 +14,8 @@ db = SQLAlchemy(app)
 def home():
     return render_template('base.html')
 
-@app.route('/players', methods=['POST', 'GET'])
-def players():
+@app.route('/test', methods=['POST', 'GET'])
+def test():
     fn=''
     ln=''
     data=''
@@ -68,9 +69,78 @@ def players():
         bd=li[3]
         country=li[5]
 
+    return render_template("test.html",fn=fn, ln=ln,bd=bd,country=country, gwon=gwon, mwon=mwon)
 
+@app.route('/versus', methods=['GET', 'POST'])
+def versus():
 
-    return render_template("players.html",fn=fn, ln=ln,bd=bd,country=country, gwon=gwon, mwon=mwon)
+    return render_template('versus.html')
+
+@app.route('/players', methods=['GET'])
+def players():
+
+    if request.method == 'GET':
+        fn=''
+        ln=''
+        data=''
+        playerID=[]
+        fn=''
+        ln=''
+        bd=''
+        country=''
+        mwon=''
+        gwon=''
+        query2 = ''
+
+        fn = request.args.get('fn')
+        ln = request.args.get('ln')
+
+        if(fn is not None):
+            #get player info
+            query = "select * from players where p_firstName = '%s' AND p_lastName = '%s';"%(fn, ln)
+            print(query)
+            conn = create_connection()
+
+            crs = conn.cursor()
+            crs.execute(query)
+
+            data = [[str(item) for item in results] for results in crs.fetchall()]
+
+            #get player ID
+            query2 = "select MIN(p_playerID) from players where p_firstName = '%s' AND p_lastName = '%s';"%(fn, ln)
+            playerID = crs.execute(query).fetchone()
+            te = []
+
+            #get titles won
+            query2 = "select COUNT(*) from allTournaments where v_wID = %d AND v_round = 'F';"%(int(playerID[0]))
+            query3 = "select COUNT(*) from allTournaments A, tournaments T where v_wID = %d AND v_round = 'F' AND t_ID = A.v_ID AND t_level = 'M';"%(int(playerID[0]))
+            query4 = "select COUNT(*) from allTournaments A, tournaments T where v_wID = %d AND v_round = 'F' AND t_ID = A.v_ID AND t_level = 'G';"%(int(playerID[0]))
+
+            twon1 = crs.execute(query2).fetchone()
+            mwon1 = crs.execute(query3).fetchone()
+            gwon1 = crs.execute(query4).fetchone()
+            twon = twon1[0]
+            mwon = mwon1[0]
+            gwon = gwon1[0]
+            print("won: " + str(twon))
+            print("mwon: " + str(mwon))
+            print("gwon: " + str(gwon))
+
+            #get grand slams won
+            #get total titles
+            close_connection(conn)
+            s = data[0]
+            print(s)
+            li = str(s).strip(']').split(", ")
+            fn =li[1]
+            ln = li[2]
+            bd=li[3]
+            country=li[5]
+        else:
+            fn = 'hello'
+            ln = 'there'
+    return render_template('players.html', fn=fn, ln=ln, bd=bd,country=country, gwon=gwon, mwon=mwon)
+
 
 def create_connection():
     conn = None
@@ -98,7 +168,14 @@ def close_connection(db):
     if db is not None:
         db.close()
 
+def main():
+    print("python main function")
 
+
+
+
+if __name__ == '__main__':
+    main()
 
 
 app.run()
