c5921956483896e0c42ae2cb72505ae6b04eedce
Anthony Choi <froshiga@gmail.com>
2019-12-07 12:39:14 -0800

    added chart to view match statistics

--- tennis.py
+++ tennis.py
@@ -2,6 +2,7 @@ import flask
 from flask import request, jsonify, url_for, render_template
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
+import pygal                                                       # First import pygal
 
 
 app = flask.Flask(__name__)
@@ -71,10 +72,56 @@ def test():
 
     return render_template("test.html",fn=fn, ln=ln,bd=bd,country=country, gwon=gwon, mwon=mwon)
 
-@app.route('/versus', methods=['GET', 'POST'])
+@app.route('/versus', methods=['GET'])
 def versus():
+    fn1 =''
+    fn2 =''
+    ln1=''
+    ln2=''
+    playerID=[]
+    if request.method == 'GET':
+        print("here")
+        if (fn1 is not None and fn2 is not None):
+            print(fn1)
+            print("now here")
 
-    return render_template('versus.html')
+            fn1 = request.args.get('fn1')
+            ln1 = request.args.get('ln1')
+            fn2 = request.args.get('fn2')
+            ln2 = request.args.get('ln2')
+            print(fn1)
+            query = "select MIN(p_playerID) from players where p_firstName = '%s' AND p_lastName = '%s';"%(fn1, ln1)
+
+            conn =create_connection()
+            crs = conn.cursor()
+
+            playerID = crs.execute(query).fetchone()
+
+            query2 = "select T.t_surface, COUNT(A.v_wID) from allTournaments A, tournaments T where A.v_wID = %d AND T.t_ID = A.v_ID group by T.t_surface;"%(int(playerID[0]))
+            results2 = crs.execute(query2)
+            surfacedata = [[str(item) for item in results] for results in crs.fetchall()]
+            for sd in surfacedata:
+                print(sd)
+
+
+            #for player 2
+
+            query = "select MIN(p_playerID) from players where p_firstName = '%s' AND p_lastName = '%s';"%(fn2, ln2)
+
+            conn =create_connection()
+            crs = conn.cursor()
+
+            playerID = crs.execute(query).fetchone()
+
+            query2 = "select T.t_surface, COUNT(A.v_wID) from allTournaments A, tournaments T where A.v_wID = %d AND T.t_ID = A.v_ID group by T.t_surface;"%(int(playerID[0]))
+            results2 = crs.execute(query2)
+            surfacedata = [[str(item) for item in results] for results in crs.fetchall()]
+            for sd in surfacedata:
+                print(sd[1])
+
+
+
+    return render_template('versus.html', fn1=fn1, fn2=fn2)
 
 @app.route('/players', methods=['GET'])
 def players():
@@ -91,10 +138,14 @@ def players():
         mwon=''
         gwon=''
         query2 = ''
+        surfacedata =[]
+        d1=[]
+        d2=[]
+        d3=[]
+
 
         fn = request.args.get('fn')
         ln = request.args.get('ln')
-
         if(fn is not None):
             #get player info
             query = "select * from players where p_firstName = '%s' AND p_lastName = '%s';"%(fn, ln)
@@ -128,6 +179,25 @@ def players():
 
             #get grand slams won
             #get total titles
+            #get surface statistics
+            query2 = "select R.year, R.surface, COUNT(A.v_wiD) from allTournaments A,(SELECT strftime('%s', t_date) AS year, t_surface as surface, t_ID as id from tournaments)R where A.v_wID = %d AND R.id = A.v_ID group by R.year, R.surface;"%('%Y', int(playerID[0]))
+            results2 = crs.execute(query2)
+
+            surfacedata = [[str(item) for item in results] for results in crs.fetchall()]
+            for sd in surfacedata:
+                #sdata.append(sd[1])
+                if sd[1] == 'Clay':
+                    d1.append(int(sd[2]))
+                elif sd[1] == 'Grass':
+                    d2.append(int(sd[2]))
+                elif sd[1] == 'Hard':
+                    d3.append(int(sd[2]))
+
+
+            print(d1)
+            print(d2)
+            print(d3)
+
             close_connection(conn)
             s = data[0]
             print(s)
@@ -136,10 +206,27 @@ def players():
             ln = li[2]
             bd=li[3]
             country=li[5]
+
+            #plotbar()
+
+
+    	    graph_data = getbardata(d1, d2, d3)
+
         else:
             fn = 'hello'
             ln = 'there'
-    return render_template('players.html', fn=fn, ln=ln, bd=bd,country=country, gwon=gwon, mwon=mwon)
+    return render_template('players.html', fn=fn, ln=ln, bd=bd,country=country, gwon=gwon, mwon=mwon, graph_data=graph_data)
+
+def getbardata(d1, d2, d3):
+    line_chart = pygal.Bar()
+    line_chart.title = 'Wins based on surface'
+    line_chart.x_labels = map(str, range(2010, 2020))
+    line_chart.add('Clay', d1)
+    line_chart.add('Grass', d2)
+    line_chart.add('Hard', d3)
+    line_chart.render()
+    return line_chart.render_data_uri()
+
 
 
 def create_connection():
@@ -151,16 +238,19 @@ def create_connection():
     return conn
 
 
-def execute_query(conn, query):
+def execute_query(query):
     if query == '' : return
 
-    #print (query)
+    conn = None
+    try:
+        conn = sqlite3.connect('/Users/anthonychoi98/Documents/GitHub/TennisDB/Tennis/Tennis.db')
+    except Error as e:
+        print(e)
 
     cursor = conn.cursor()
 
     results = cursor.execute(query).fetchall()
-    for result in results:
-        print(result)
+    conn.commit()
 
     return results
 
@@ -168,9 +258,15 @@ def close_connection(db):
     if db is not None:
         db.close()
 
+def plot():
+    bar_chart = pygal.Bar()                                            # Then create a bar graph object
+    bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])  # Add some values
+    bar_chart.render_to_file('bar_chart.svg')
+    b_data = bar_chart.render_data_uri()
+    return render_template("players.html", url = b_data)
+
 def main():
     print("python main function")
-
 
 
 
